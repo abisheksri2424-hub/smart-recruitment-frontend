7b5bb0c6adb2319aed1970e99f2238b899767252
Srikanthan Sriabishek <UT011141@unicomtic.com>
2026-09-05 16:14:31 +0530

    Initial Angular frontend structure

---
+++ src/app/components/applicant-card/applicant-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicant-card',
+  imports: [],
+  templateUrl: './applicant-card.component.html',
+  styleUrl: './applicant-card.component.css'
+})
+export class ApplicantCardComponent {
+
+}
