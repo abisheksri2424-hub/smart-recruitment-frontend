7c403119554ed1bfaf56d40933efe61dc5e3da20
Ragavan Komaruthan <UT011622@unicomtic.com>
2026-09-11 13:10:37 +0530

    Complete seeker jobs static UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './seeker/dashboard/dashboard.component';
+import { JobsComponent } from './seeker/jobs/jobs.component';
+import { ApplicationsComponent } from './seeker/applications/applications.component';
+import { ProfileComponent } from './seeker/profile/profile.component';
+
+export const routes: Routes = [
+     {
+    path: 'seeker/dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'seeker/jobs',
+    component: JobsComponent
+  },
+  {
+    path: 'seeker/applications',
+    component: ApplicationsComponent
+  },
+  {
+    path: 'seeker/profile',
+    component: ProfileComponent
+  }
+];
--- src/app/seeker/jobs/jobs.component.ts
+++ src/app/seeker/jobs/jobs.component.ts
@@ -2,10 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-jobs',
+  standalone: true,
   imports: [],
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.css'
 })
 export class JobsComponent {
+
+  jobs = [
+    {
+      id: 1,
+      title: 'Software Developer',
+      company: 'ABC Company',
+      location: 'Jaffna',
+      matchScore: 85
+    },
+    {
+      id: 2,
+      title: 'Frontend Developer',
+      company: 'Tech Solutions',
+      location: 'Colombo',
+      matchScore: 78
+    }
+  ];
 
 }
